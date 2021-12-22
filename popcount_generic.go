bb0511f64f878e4a200b0d3977467409106efb22
Christopher Hlubek <christopher.hlubek@networkteam.com>
2021-12-22 08:48:19 +0100

    Make code compile on non-amd64

--- popcount_generic.go
+++ popcount_generic.go
@@ -4,12 +4,12 @@ package bits
 
 import "math/bits"
 
-func popcountInit() {}
+func popcountInit() bool { return false }
 
-func PopcountIsIntrinsic() bool { return false }
-func PopcountUint(v uint) int { return bits.OnesCount(v) }
+func PopcountIsIntrinsic() bool   { return false }
+func PopcountUint(v uint) int     { return bits.OnesCount(v) }
 func PopcountUint64(v uint64) int { return bits.OnesCount64(v) }
 
 func Bitindex(bmap, bitpos uint) int {
-  return PopcountUint(bmap & (bitpos - 1))
+	return PopcountUint(bmap & (bitpos - 1))
 }
